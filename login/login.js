814bf0275902751a50e6e8c2d366f7000f0abd33
ko-math <sorehakiemasu@gmail.com>
2026-08-14 07:27:51 +0900

    Update login.js

--- login/login.js
+++ login/login.js
@@ -53,17 +53,6 @@ function generateCode(list){
   return check;
 }
 
-const send1 = document.querySelector('#nameSend');
-send1.addEventListener('click',async function(){
-  const userName = document.querySelector('#userName').value;
-  const er = await getUser(userName);
-  if(er){
-    window.alert('有効なユーザー名を入力してください。')
-    return 0;
-  }
-  location.assign('./check.html');
-});
-
 async function getUser(u){
   //window.alert('debug');
   const res = await fetch('https://trampoline.turbowarp.org/api/users/' + u);
