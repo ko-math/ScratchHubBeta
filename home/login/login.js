28cb6c99eaa47780e46f4dc5c57d69059c0eee3d
ko-math <sorehakiemasu@gmail.com>
2026-08-13 12:26:34 +0900

    Update login.js

--- home/login/login.js
+++ home/login/login.js
@@ -10,10 +10,19 @@ for(let i = 0;i < 10;i++){
 }
 usable.push('-');
 usable.push('_');
-alert(usable);
+let check = '';
+let checkLength = random(5,10);
+for(let i = 0;i < checkLength;i++){
+  check += usable[random(0,usable.length)] 
+}
+
 
 const send1 = document.querySelector('#nameSend');
 send1.addEventListener('click',function(){
   const userName = document.querySelector('#userName').value;
   alert(userName);
 });
+
+funtion random(a,b){
+  return Math.floor(a + (b-a) * Math.random());
+}
