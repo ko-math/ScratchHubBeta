ac9ad4c21e2aa58420280cd6bf6de13a0501e1f8
ko-math <sorehakiemasu@gmail.com>
2026-08-13 12:38:40 +0900

    Update login.js

--- home/login/login.js
+++ home/login/login.js
@@ -1,22 +1,4 @@
-const usable = [];
-for(let i = 65;i <= 90;i++){
-  usable.push(String.fromCodePoint(i));
-}
-for(let i = 97;i <= 122;i++){
-  usable.push(String.fromCodePoint(i));
-}
-for(let i = 0;i < 10;i++){
-  usable.push(i.toString());
-}
-usable.push('-');
-usable.push('_');
-let check = '';
-let checkLength = random(5,10);
-for(let i = 0;i < checkLength;i++){
-  check += usable[random(0,usable.length - 1)] 
-}
-alert(check);
-
+alert(generateCode(init(),10,15));
 
 const send1 = document.querySelector('#nameSend');
 send1.addEventListener('click',function(){
@@ -24,6 +6,30 @@ send1.addEventListener('click',function(){
   alert(userName);
 });
 
+//関数
+function init(){
+  const list = [];
+  for(let i = 65;i <= 90;i++){
+    list.push(String.fromCodePoint(i));
+  }
+  for(let i = 97;i <= 122;i++){
+    list.push(String.fromCodePoint(i));
+  }
+  for(let i = 0;i < 10;i++){
+    list.push(i.toString());
+  }
+  list.push('-');
+  list.push('_');
+  return list;
+}
 function random(a,b){
   return Math.floor((b - a + 1) * Math.random()) + a;
+}
+function generateCode(list,a,b){
+  let check = '';
+  let checkLength = random(a,b);
+  for(let i = 0;i < checkLength;i++){
+    check += list[random(0,list.length - 1)] 
+  }
+  return check;
 }
