aa6d81d6892e06166fc3bd44247a4f6b7b4d4b9a
ko-math <sorehakiemasu@gmail.com>
2026-08-13 12:09:51 +0900

    Update login.js

--- home/login/login.js
+++ home/login/login.js
@@ -2,3 +2,9 @@ const usable = [];
 fot(let i = 65;i <= 90;i++){
   usable.push(String.fromCodePoint(i));
 }
+alert(usable);
+
+const send1 = document.querySelector('#nameSend');
+send1.addEventListener('click',function(){
+  const userName = document.querySelector('#userName').value;
+});
