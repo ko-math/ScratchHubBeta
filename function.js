f69fa5bc0f5478199bd3f08d3ba175ace8a63ae8
ko-math <sorehakiemasu@gmail.com>
2026-08-13 18:24:33 +0900

    Create function.js

---
+++ function.js
@@ -0,0 +1,8 @@
+function addCopyButton(parent,copyText){
+  const b = document.createElement('button');
+  b.textContent = 'copy';
+  parent.append(b);
+  b.addEventListener('click',function (){
+    navigator.clipboard.writeText(text);
+  });
+}
