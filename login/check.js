ed60577849b47d46e2a284ea365a7e0d31ffb46e
ko-math <sorehakiemasu@gmail.com>
2026-08-13 22:00:18 +0900

    Update check.js

--- login/check.js
+++ login/check.js
@@ -1,10 +1,11 @@
+import {userName} from './user.js';
 const div = document.querySelector('#input');
 const checkCode = document.createElement('p');
 checkCode.textContent = generateCode(init(),10,15);
 checkCode.classList.add('codeText');
 div.append(checkCode);
 addCopyButton(div,checkCode.textContent);
-getUser('ko-math');
+getUser(userName);
 
 //関数
 function init(){
