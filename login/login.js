f710d7601471c378becf4388eece81aa009c1811
ko-math <sorehakiemasu@gmail.com>
2026-08-14 18:55:06 +0900

    Update login.js

--- login/login.js
+++ login/login.js
@@ -1,6 +1,5 @@
 const send1 = document.querySelector('#nameSend');
-send1.addEventListener('click',async function(){ 
-  window.alert(1);
+send1.addEventListener('click',async function(){
   const div = document.querySelector('#input');
   
   const userNameInput = document.querySelector('#userName');
