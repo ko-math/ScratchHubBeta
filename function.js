5ee8749b206371a18d46e6b584561fc38ffb0e35
ko-math <sorehakiemasu@gmail.com>
2026-08-15 07:58:13 +0900

    Update function.js

--- function.js
+++ function.js
@@ -1,7 +1,8 @@
 //copyButton
-function addCopyButton(parent,copyText){
+function addCopyButton(parent,copyText,id){
   const b = document.createElement('button');
   b.textContent = 'copy';
+  b.id = id;
   parent.append(b);
   b.addEventListener('click',function (){
     navigator.clipboard.writeText(copyText);
