0f209ff36546b5a5a70f5f857db656a33f70df13
ko-math <sorehakiemasu@gmail.com>
2026-08-14 17:09:19 +0900

    Update common.js

--- common.js
+++ common.js
@@ -1,1 +1,9 @@
-
+//header
+(() => {
+  const div = document.querySelector('#header');
+  const header = document.createElement('header');
+  header.innerHTML = `
+    <h2>募集</h2>
+  `;
+  div.append(header);
+})();
