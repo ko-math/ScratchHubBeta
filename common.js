09569184511fb9a9949d968d1148afd1e25f93c9
ko-math <sorehakiemasu@gmail.com>
2026-08-15 10:30:28 +0900

    Update common.js

--- common.js
+++ common.js
@@ -11,5 +11,5 @@
 
 const button = document.querySelector('#login'); //遅.getElementById
 button.addEventListener('click',function(){
-  location.assign('./login/login.html');                    
+  location.assign('https://ko-math.github.io/ScratchTechnologyNote/login/login.html');                    
 });
