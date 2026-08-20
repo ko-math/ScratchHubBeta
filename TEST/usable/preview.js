a66196f30341b81e3fcaeb18a87727609edb8e32
ko-math <sorehakiemasu@gmail.com>
2026-08-20 15:44:42 +0900

    Create preview.js

---
+++ TEST/usable/preview.js
@@ -0,0 +1,12 @@
+const head = document.querySelector('head');
+const body = document.querySelector('body');
+const headContent = `
+  <title>hello</title>
+  <meta charset="utf-8">
+`; 
+const bodyContent = `
+  <h1>An article</h1>
+  <p>Hello,World!</p>
+`;
+head.insertAdjacentHTML('beforeend',headContent);
+body.insertAdjacentHTML('beforeend',bodyContent);
